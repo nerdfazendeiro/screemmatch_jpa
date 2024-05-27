c024979c41435495b984bc088da0e906408942b6
Rafael Ramalho Silveira <rafael_silveira125@hotmail.com>
2024-05-27 05:53:30 -0400

    Mapeando as entidades

--- src/main/java/br/com/rafaelsilveiradev/model/Serie.java
+++ src/main/java/br/com/rafaelsilveiradev/model/Serie.java
@@ -2,10 +2,27 @@ package br.com.rafaelsilveiradev.model;
 
 import java.util.OptionalDouble;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String poster;
@@ -21,6 +38,14 @@ public class Serie {
         this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     public String getTitulo() {
         return titulo;
     }
@@ -82,6 +107,5 @@ public class Serie {
         return "Serie genero=" + genero + ", titulo=" + titulo + ", totalTemporadas=" + totalTemporadas + ", avaliacao=" + avaliacao
                 + ", atores=" + atores + ", poster=" + poster + ", sinopse=" + sinopse;
     }
-
     
 }
